9838df0cdf1437866395adbbb0c4adeca173ad02
IsaacArboleda29 <naranjojaves1004@gmail.com>
2024-07-08 00:12:43 -0700

    act terminada

---
+++ taller_javascript/js/script.js
@@ -0,0 +1,14 @@
+
+function cargar_elementos() {
+    var opcion_1 = document.getElementById("opcion_01");
+
+    
+    opcion_1.style.display = "block";
+}
+
+function eliminar_elementos() {
+    var opcion_1 = document.getElementById("opcion_01");
+
+   
+    opcion_1.style.display = "none";
+}
